e6a223d04f18d11bd7cfd983aa91a8c403a87f83
Omer Mualem <omer2313@gmail.com>
2022-08-05 16:21:23 +0300

    Added day4 project
    This project is a rock-papers-scissors game. The player chooses rock, papers or scissors and the computer randomizes his choice and then the program outputs who wins.

---
+++ day4-rock-papers-scissors.py
@@ -0,0 +1,46 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+options = [rock, paper,scissors]
+player_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for scissors.\n"))
+pc_input = random.randint(0,2)
+
+#print player choice
+print(options[player_input])
+
+#print computer choice
+print(f"Computer chose:\n{options[pc_input]}")
+
+if(player_input > pc_input):
+    print("You win")
+elif(player_input == pc_input):
+    print("Tie")
+else:
+    print("You lose")
