2a53bc75c94e50dd9d1d5b55623d59fd9294326a
Omer Mualem <omer2313@gmail.com>
2022-09-19 15:24:43 +0300

    Added day 27 project

--- day27-mile_to_km/main.py
+++ day27-mile_to_km/main.py
@@ -1,18 +1,34 @@
 from tkinter import *
+from turtle import color
+from unittest import TextTestResult
 
 
 window = Tk()
-window.minsize(width=400, height=300)
+window.title("Miles To Kilometer Converter")
+window.minsize(width=200, height=70)
+window.config(padx=10,pady=20)
 
-label = Label(text="I Am A Label", font=("Arial", 24, "bold"))
-label.pack()
+entry = Entry(width=10)
+entry.grid(column=1,row=0)
+
+label_miles = Label(text="Miles")
+label_miles.grid(column=2,row=0)
+
+label_answer = Label(text="0")
+label_answer.grid(column=1,row=1)
+
+label_km = Label(text="Km")
+label_km.grid(column=2,row=1)
+
+label = Label(text="is equal to" )
+label.grid(column=0, row=1)
 
 
-def button_clicked():
-    label["text"] = "Hello"
-    label.pack()
+def button_func():
+    label_answer["text"] = round(float(entry.get()) * 1.609344, 2)
 
-button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button = Button(text="Calculate", command=button_func)
+button.grid(column=1, row=2)
+
 
 window.mainloop()
